239293ea6d5fcc1c10afc38c1a407b79b00fe597
Sven Pfaller <sven@inrain.org>
2026-05-02 01:32:43 +0200

    MERGE bolerplate: eslint config + stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig(
-    js.configs.recommended,
-    {
-        linterOptions: { reportUnusedDisableDirectives: true },
-        rules: { "no-undef": "off" }
-    }
+  js.configs.recommended,
+  // https://prettier.io/docs/rationale
+  stylistic.configs.customize({ braceStyle: "1tbs", semi: true, quotes: "double" }),
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    rules: { "no-undef": "off" },
+  },
 );
