80af3feb096aa4512ce993363a26b876d32426c4
Sven Pfaller <sven@inrain.org>
2026-05-07 21:47:03 +0200

    WIP canvas

--- src/discman/index.js
+++ src/discman/index.js
@@ -1,5 +1,25 @@
 /** Disc-Man. */
 
+import p5 from "p5";
+
 /** ... */
-class Game extends HTMLElement {}
+class Game extends HTMLElement {
+  /**
+   * ...
+   * @type {p5} p
+   */
+  p;
+
+  constructor() {
+    super();
+    this.p = new p5((p) => {
+      p.setup = () => {
+        p.createCanvas(640, 360);
+      };
+
+      p.draw = () => {
+      };
+    });
+  }
+}
 customElements.define("discman-game", Game);
