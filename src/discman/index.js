1a3734a27eeb73275bce70a89ba6a91200af9d71
Sven Pfaller <sven@inrain.org>
2026-05-07 23:15:19 +0200

    WIP grid

--- src/discman/index.js
+++ src/discman/index.js
@@ -1,7 +1,7 @@
 /** Disc-Man. */
 
 import p5 from "p5";
-import { NEON_PALETTE, Rectangle, transparent, variable } from "#sticky";
+import { NEON_PALETTE, Rectangle, h, point, px, subtract, transparent, variable } from "#sticky";
 
 /**
  * @type {Game}
@@ -14,15 +14,91 @@ class Screen {
   render() {}
 }
 
+/**
+ * ...
+ */
+class Entity {
+  /**
+   * ...
+   * @type {number}
+   */
+  x = 0;
+  /**
+   * ...
+   * @type {number}
+   */
+  y = 0;
+  /**
+   * ...
+   * @type {import("#sticky").Shape}
+   */
+  model;
+
+  /**
+   * @param {import("#sticky").Shape} model
+   */
+  constructor(model) {
+    this.model = model;
+  }
+
+  /**
+   * ...
+   * @param {number} x - ...
+   * @param {number} y - ...
+   */
+  moveTo(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/**
+ * ...
+ */
+class Cell extends Entity {
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(x, y) {
+    super(
+      new Rectangle(
+        subtract(h(1 / World.GRID_SIZE), px(5)), subtract(h(1 / World.GRID_SIZE), px(5)),
+        point(h((x + 1 / 2) / World.GRID_SIZE), h((y + 1 / 2) / World.GRID_SIZE)),
+        { stroke: variable("darkGray", "color") },
+      ),
+    );
+    this.moveTo(x, y);
+  }
+}
+
 /** ... */
 class World extends Screen {
+  /**
+   * ...
+   * @type {Cell[][]}
+   */
+  grid;
+
   /** @type {Rectangle} */
   #model;
 
+  static GRID_SIZE = 10;
+
   constructor() {
     super();
+    this.grid = [...Array(World.GRID_SIZE).keys()].map(
+      y => [...Array(World.GRID_SIZE).keys()].map(x => new Cell(x, y)),
+    );
+
     this.#model = new Rectangle(
       { variables: { ...NEON_PALETTE }, fill: variable("black", "color"), stroke: transparent() },
+
+      // Grid
+      new Rectangle(
+        h(1),
+        ...this.grid.flatMap(row => row.map(cell => cell.model)),
+      ),
     );
   }
 
